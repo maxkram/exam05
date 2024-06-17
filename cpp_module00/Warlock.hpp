1f888e539354456057ffa7f7863f22b7d9bc20f5
maxkram <max.kramer@live.com>
2024-06-16 23:38:26 -0400

    exercised 16.06.24

--- cpp_module00/Warlock.hpp
+++ cpp_module00/Warlock.hpp
@@ -1,24 +1,48 @@
+// #pragma once
+
+// #include <iostream>
+
+// class Warlock
+// {
+//     private:
+//         // private data members
+//         std::string name;
+//         std::string title;
+//         // default constructor 
+//         Warlock();
+//         // copy constructor
+//         Warlock(Warlock const &other);
+//         // assignment operator
+//         Warlock &operator=(Warlock const &other);
+//     public:
+//         // constructor
+//         Warlock(std::string const &name, std::string const &title);
+//         // destructor
+//         ~Warlock();
+//         // accessors
+//         std::string const &getName() const;
+//         std::string const &getTitle() const;
+//         // mutator
+//         void setTitle(std::string const &title);
+
+//         void introduce() const;
+// };
+
 #pragma once
-
 #include <iostream>
-
 class Warlock
 {
     private:
         std::string name;
         std::string title;
-
         Warlock();
         Warlock(Warlock const &other);
         Warlock &operator=(Warlock const &other);
     public:
         Warlock(std::string const &name, std::string const &title);
         ~Warlock();
-
         std::string const &getName() const;
         std::string const &getTitle() const;
-
         void setTitle(std::string const &title);
-
         void introduce() const;
 };
