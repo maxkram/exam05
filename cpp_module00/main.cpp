ee8811749425ada5aeb90ea0c35e90ab12a0542d
maxkram <max.kramer@live.com>
2024-06-25 01:04:15 -0400

    Reformat ex00

--- cpp_module00/main.cpp
+++ cpp_module00/main.cpp
@@ -1,17 +1,16 @@
 #include "Warlock.hpp"
 
-int main()
-{
-	Warlock const richard("Richard", "Mistress of Magma");
-	richard.introduce();
-	std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+int main() {
+    Warlock const richard("Richard", "Mistress of Magma");
+    richard.introduce();
+    std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
 
-	Warlock* jack = new Warlock("Jack", "the Long");
-	jack->introduce();
-	jack->setTitle("the Mighty");
-	jack->introduce();
+    Warlock *jack = new Warlock("Jack", "the Long");
+    jack->introduce();
+    jack->setTitle("the Mighty");
+    jack->introduce();
 
-	delete jack;
+    delete jack;
 
-	return (0);
+    return (0);
 }
--- cpp_module01/ATarget.hpp
+++ cpp_module01/ATarget.hpp
@@ -9,7 +9,7 @@
 // {
 // 	private:
 // 		std::string type;
-	
+
 // 	public:
 // 		ATarget();
 // 		ATarget(std::string const &type);
@@ -25,19 +25,29 @@
 // };
 
 #pragma once
+
 #include "ASpell.hpp"
 #include <iostream>
+
 class ASpell;
+
 class ATarget {
-	private:
-		std::string type;
-	public:
-		ATarget();
-		ATarget(std::string const &type);
-		ATarget(ATarget const &other);
-		ATarget &operator=(ATarget const &other);
-		virtual ~ATarget();
-		std::string  const &getType() const;
-		void getHitBySpell(ASpell const &aspell_ref) const;
-		virtual ATarget *clone() const = 0;
+private:
+    std::string type;
+public:
+    ATarget();
+
+    ATarget(std::string const &type);
+
+    ATarget(ATarget const &other);
+
+    ATarget &operator=(ATarget const &other);
+
+    virtual ~ATarget();
+
+    std::string const &getType() const;
+
+    void getHitBySpell(ASpell const &aspell_ref) const;
+
+    virtual ATarget *clone() const = 0;
 };
